dbaef631ac964bd89c567c45c1d581203c11a123
Kartik Saini <kartiksaini2004@gmail.com>
2025-03-22 09:52:09 +0530

    PPVF

---
+++ OUTPUT/printOutput.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# Load the array from the .npy file
+array = np.load('/home/kartik/Desktop/PPVF/OUTPUT/final_state/final_hitrate/01-21-18-04-51-PPVF-HPP_PAC-training-Noise_True-fe_4-ce_[0.01]-xi_15.0-epsilon_1.0-dataset_1.0.npy')
+
+print(array)
